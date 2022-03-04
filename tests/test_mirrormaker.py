ec6d63478fcd9893efc8151a2e7c422f0560156e
smheidrich <smheidrich@weltenfunktion.de>
2022-03-04 23:16:40 +0100

    Fix one old test
    How much has to be mocked here shows how garbage my code is =>
    refactor!!

--- tests/test_mirrormaker.py
+++ tests/test_mirrormaker.py
@@ -1,24 +1,43 @@
+from collections import UserList
 from dataclasses import dataclass
 import responses
 import mirrormaker
 from mirrormaker import github
 from mirrormaker import gitlab
 import pytest
+from unittest.mock import Mock
 
 
-@pytest.mark.xfail(strict=True, reason="API wrapper libs request differently")
 @responses.activate
-def test_filter_forked_repos():
-    resp_json = [{'name': 'repo_1', 'fork': True},
-                 {'name': 'repo_2', 'fork': False}]
+def test_filter_forked_repos(monkeypatch):
+    repo1 = Mock()
+    repo1.configure_mock(name="repo_1", fork=True)
+    repo2 = Mock()
+    repo2.configure_mock(name="repo_2", fork=False)
+    class MockRepoIter(UserList):
+        @property
+        def totalCount(self):
+            return len(self.data)
+    repos = MockRepoIter([repo1, repo2])
+    token = "some-token"
+    MockGithub = Mock()
+    mock_get_repos = MockGithub(token).get_user().get_repos
+    mock_get_repos.side_effect = lambda *a, **kw: repos
 
-    responses.add(responses.GET, 'https://api.github.com:443/user/repos?type=public',
-                  json=resp_json, status=200)
+    with monkeypatch.context() as m:
+        m.setattr(github, "Github", MockGithub)
+        m.setattr(github, "token", token)
 
-    github_repos = github.get_repos()
+        github_repos = github.get_repos()
 
-    assert len(github_repos) == 1
-    assert github_repos[0].name == 'repo_2'
+        assert len(github_repos) == 1
+        assert github_repos[0].name == 'repo_2'
+
+        m.setattr(github, "target_forks", False)
+
+        github_repos = github.get_repos()
+
+        assert len(github_repos) == 1
 
 
 @pytest.mark.xfail(strict=True, reason="API wrapper libs request differently")
